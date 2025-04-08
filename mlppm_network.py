ea5325209ad5165572e5ee273828bc700d8df6f6
zhh2001 <1652709417@qq.com>
2025-04-08 19:52:22 +0800

    上传ML方案的网络脚本

---
+++ mlppm_network.py
@@ -0,0 +1,21 @@
+def main():
+    """ML方案的网络"""
+    net = NetworkAPI()
+
+    net.setCompiler(p4rt=True)
+    net.addP4RuntimeSwitch(name='s1')
+    net.setP4SourceAll(p4_src='mlppm.p4')
+
+    net.addHost(name='h1')
+    net.addHost(name='h2')
+
+    net.addLink(node1='s1', node2='h1')
+    net.addLink(node1='s1', node2='h2')
+    net.setBwAll(10)
+
+    net.mixed()
+    net.setLogLevel(logLevel='info')
+    net.enablePcapDumpAll(pcap_dir='./pcap')
+    net.enableLogAll(log_dir='./log')
+    net.enableCli()
+    net.startNetwork()
